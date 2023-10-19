d8e5710533c582dd62c55cb5fd9965fb293b3329
Armolas <armolas06@gmail.com>
2023-10-19 23:35:11 +0000

    fixed rotr

--- rotr.c
+++ rotr.c
@@ -13,13 +13,14 @@ void rotr(stack_t **stack, unsigned int line_number)
 	stack_t *end = *stack;
 	(void) line_number;
 	
-	if (!(*stack) || (*stack)->next != NULL)
+	if (!(*stack) || (*stack)->next == NULL)
 		return;
 
-	while ((*stack)->next != NULL)
+	while (end->next != NULL)
 		end = end->next;
 	end->prev->next = NULL;
 	end->prev = NULL;
 	end->next = *stack;
+	(*stack)->prev = end;
 	*stack = end;
 }
