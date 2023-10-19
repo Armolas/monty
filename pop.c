64497835e03f721a0bcdd7d8e5b031e3ed91b423
Armolas <armolas06@gmail.com>
2023-10-19 11:52:18 +0000

    fixed pop

--- pop.c
+++ pop.c
@@ -10,7 +10,7 @@ void pop(stack_t **stack, unsigned int line_number)
 
 	if (!temp)
 	{
-		dprintf(2, "L%d: can't pop an empty stack", line_number);
+		dprintf(2, "L%d: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	*stack = (*stack)->next;
